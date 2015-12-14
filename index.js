1274fecd6950add1d9120e407088720de8952f18
Max Ogden <max@maxogden.com>
2015-12-14 12:06:10 -0800

    remove mkdirp, as tar-fs already does it

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 var tar = require('tar-fs')
-var mkdirp = require('mkdirp')
 var pump = require('pump')
 
 module.exports = function (src, dest, cb) {
-  mkdirp(dest, function (err) {
-    if (err) return cb(err)
-    var pack = tar.pack(src)
-    var unpack = tar.extract(dest)
-    pump(pack, unpack, cb)
-  })
+  var pack = tar.pack(src)
+  var unpack = tar.extract(dest)
+  pump(pack, unpack, cb)
 }
